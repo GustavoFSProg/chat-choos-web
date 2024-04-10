b642e6d0bccd0304db09369ec725e86fcdf20d63
GusFSProg <gustavo.prog40@gmail.com>
2024-04-10 08:59:45 -0300

    container full all

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom'
-import { Container, ContainerMiddle } from './styled-app'
+// import { Container, ContainerMiddle } from './styled-app'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import './index.css'
 
 function App() {
   return (
-    <Container>
-      <ContainerMiddle>
-        <h2>APP HOME CHAT</h2>
-        <Link to="/chat">CHAT</Link>
-      </ContainerMiddle>
-    </Container>
+    <>
+      <h2>APP HOME CHAT</h2>
+      <Link to="/chat">CHAT</Link>
+    </>
   )
 }
 
--- src/router.tsx
+++ src/router.tsx
@@ -6,19 +6,22 @@ import Login from './pages/Login'
 // import { Container } from 'react-bootstrap'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import { Container } from 'react-bootstrap'
+import { Container, ContainerMiddle } from './styled-app'
 
 function Routers() {
   return (
-    // <Container className="text-secondary">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<App />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-    // </Container>
+    <Container>
+      <ContainerMiddle>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" exact element={<App />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </ContainerMiddle>
+    </Container>
   )
 }
 
